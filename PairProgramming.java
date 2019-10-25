8ae19797cecfd2fc167799d251113e651263fc5f
kklymets1998 <kklymets.ieu2017@student.ie.edu>
2019-10-25 12:58:23 +0200

    PairProgramming file modified

--- PairProgramming.java
+++ PairProgramming.java
@@ -9,3 +9,11 @@ public class PairProgramming {
         int count = 1;
         File file = new File("/Users/katerinaklymets/Desktop/unsorteddict.txt");
         LinkedList<String> list = new LinkedList<String>();
+
+        try {
+            Scanner sc = new Scanner(new FileInputStream(file));
+            while (sc.hasNextLine()) {
+                content = sc.nextLine();
+                list.add(content);
+            }
+            
